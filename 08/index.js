97aef847e5fbd88684919ca6138caa33cbdba3b7
Daniil Nikolaev <daniil.nikolaev.2013@gmail.com>
2022-05-19 12:58:24 +0700

    update_08_task

--- 08/index.js
+++ 08/index.js
@@ -1,5 +1,28 @@
 function sum(a, b){
-  return (BigInt(a) + BigInt(b)).toString()
+  a = a.toString();
+  b = b.toString();
+
+  const aLen = a.length
+  const bLen = b.length
+  const maxLen = Math.max(aLen, bLen)
+
+  let carry = 0, sum = ''
+
+  for (let i = 1; i <= maxLen; i++) {
+    let A = +a.charAt(aLen - i)
+    let B = +b.charAt(bLen - i)
+
+    let t = carry + A + B
+    carry = t/10 |0
+    t %= 10
+
+    sum = (i === maxLen && carry)
+      ? carry*10 + t + sum
+      : t + sum
+  }
+
+  return sum
+  // return (BigInt(a) + BigInt(b)).toString() // Старый вариант решения
 }
 
 module.exports = sum;
